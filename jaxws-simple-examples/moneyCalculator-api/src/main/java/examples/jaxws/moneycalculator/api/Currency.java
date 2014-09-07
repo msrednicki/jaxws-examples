284a7c7e2d467ecec3c74ed6782f5ee6c0490df5
marcin <marcin@192.168.1.104>
2014-09-07 15:09:12 +0200

    cleaning :/

--- jaxws-simple-examples/moneyCalculator-api/src/main/java/examples/jaxws/moneycalculator/api/Currency.java
+++
@@ -1,7 +0,0 @@
-package examples.jaxws.moneycalculator.api;
-
-public enum Currency {
-
-    PLN, EUR, CHF, USD, GBP;
-    
-}
--- jaxws-simple-examples/moneyCalculator-api/src/main/java/examples/jaxws/moneycalculator/api/MoneyCalculator.java
+++
@@ -1,9 +0,0 @@
-package examples.jaxws.moneycalculator.api;
-
-import org.joda.time.LocalDate;
-
-public interface MoneyCalculator {
-
-    MoneyAmount convertMoney(MoneyAmount moneyAmount, Currency outputCurrency, LocalDate exchangeRateDate);
-    
-}
